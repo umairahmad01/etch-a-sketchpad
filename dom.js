fe133b0f5fafa4d0d14294e680eea7c0a4799e9f
Umair <umairahmad1599@gmail.com>
2025-10-20 21:06:09 +0500

    Fix the issue of only being able to do 16x16 grid of square.Add hover effect when mouse enter the element.

--- dom.js
+++ dom.js
@@ -1,17 +1,26 @@
-const whole = document.querySelector("body");
+const body = document.querySelector("body");
 const div = document.querySelector(".container");
 div.style.display = "flex";
-div.style.flexWrap = "wrap";
-div.style.width = "960px"
-whole.appendChild(div);
+div.style.width = "1000px";
 let grid = 16;
 for (let i = 0; i < grid;i++) { 
-  for ( let j = 0;j < grid;j++) {
+  const square = document.createElement("div");
+  square.style.display = "flex";
+  square.style.width = "1000px";
+  body.appendChild(square);
+  for (let j = 0;j < grid; j++){
     const box = document.createElement("div");
-    div.appendChild(box);
+    square.appendChild(box);
     box.textContent = `${j}`;
     box.style.height = "56px";
     box.style.width = "56px";
     box.style.border = "2px solid black";
-  }
+    box.addEventListener("mouseenter", function(e) {
+      box.style.backgroundColor = "red";
+    })
+    box.addEventListener("mouseleave", function(e) {
+      box.style.backgroundColor = "white";
+    })
+ }
 }
+
